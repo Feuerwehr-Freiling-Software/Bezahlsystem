6c5523c4bbdfcbdc033f33aac72aee54eabe9cf2
HaunBa <haunschmied.b@gmail.com>
2022-11-07 12:46:46 +0100

    Added Server and API

--- Coffeshopper/Server/Program.cs
+++ Coffeshopper/Server/Program.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Server.Config;
 using Server.Data;
+using System.Runtime.CompilerServices;
+
+var seed = args.Contains("/seed");
+if (seed)
+{
+    args = args.Except(new[] { "/seed" }).ToArray();
+}
 
 var builder = WebApplication.CreateBuilder(args);
+var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (seed)
+{
+    SeedData.EnsureSeedData(defaultConnString);
+}
 
 var assembly = typeof(Program).Assembly.GetName().Name;
 
-var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
 {
@@ -30,7 +43,19 @@ builder.Services.AddIdentityServer()
         })
         .AddDeveloperSigningCredential();
 
+builder.Services.AddAuthorization();
+builder.Services.AddControllersWithViews();
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
+app.UseStaticFiles();
+app.UseRouting();
+app.UseIdentityServer();
+app.UseAuthorization();
+app.UseEndpoints(opt =>
+{
+    opt.MapDefaultControllerRoute();
+});
 
 app.Run();
