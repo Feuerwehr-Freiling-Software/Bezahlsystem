77d0694dad68e04b9d138c2a1d4d4a6491a343bc
HaunBa <haunschmied.b@gmail.com>
2022-12-14 14:09:09 +0100

    Reset

--- Bezahlsystem/Bezahlsystem/Server/Models/ArtType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bezahlsystem.Server.Models
-{
-    public enum ArtType
-    {
-        Food = 1,
-        Drink = 2,
-        Other = 3
-    }
-}
--- Coffeshopper/Blazor-Client/Services/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿using IdentityModel.Client;
-
-namespace Blazor_Client.Services
-{
-    public interface ITokenService
-    {
-        Task<TokenResponse> GetToken(string scope);
-    }
-}
