d9e5620305bdf6fb38ba0ff76cc85086c19d49e8
HaunBa <haunschmied.b@gmail.com>
2022-12-14 15:22:21 +0100

    Added PaymentSystem API Projects

---
+++ Paymentsystem.API/Paymentsystem.Shared/Models/Price.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Paymentsystem.Shared.Models
+{
+    public partial class Price
+    {
+        public Price()
+        {
+            Articles = new HashSet<Article>();
+            BoughtArticles = new HashSet<BoughtArticle>();
+        }
+
+        public int PriceId { get; set; }
+        public DateTime Since { get; set; }
+        public DateTime Until { get; set; }
+        public double Amount { get; set; }
+
+        public virtual ICollection<Article> Articles { get; set; }
+        public virtual ICollection<BoughtArticle> BoughtArticles { get; set; }
+    }
+}
---
+++ Paymentsystem.API/Paymentsystem.Shared/Models/TopUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Paymentsystem.Shared.Models
+{
+    public partial class TopUp
+    {
+        public int TopUpId { get; set; }
+        public string PersonId { get; set; } = null!;
+        public DateTime Date { get; set; }
+        public string Description { get; set; } = null!;
+        public double CashAmount { get; set; }
+        public string ExecutorId { get; set; } = null!;
+
+        public virtual User Executor { get; set; } = null!;
+        public virtual User Person { get; set; } = null!;
+    }
+}
