512b618ce98eaf703ffb4fc8270c19a50e6a88f1
HaunBa <haunschmied.b@gmail.com>
2022-12-15 15:59:27 +0100

    Added Project

---
+++ Paymentsystem/Paymentsystem/Client/AuthStateProvider.cs
@@ -0,0 +1,35 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Paymentsystem.Client
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService localStorageService;
+
+        public AuthStateProvider(ILocalStorageService localStorageService)
+        {
+            this.localStorageService = localStorageService;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var state = new AuthenticationState(new ClaimsPrincipal());
+
+            string username = await localStorageService.GetItemAsStringAsync("username");
+            if(!string.IsNullOrEmpty(username) )
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, username )
+                }, "Test authentication type");
+
+                state = new AuthenticationState(new ClaimsPrincipal(identity));
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+    }
+}
