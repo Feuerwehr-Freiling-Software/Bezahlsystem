edabb4eded5a11f76b72e3cf1932efdd568b8fe1
HaunBa <haunschmied.b@gmail.com>
2022-12-19 22:41:04 +0100

    Added Products Page

---
+++ Paymentsystem/Paymentsystem/Client/Pages/Products.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Paymentsystem.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Paymentsystem.Client.Pages
+{
+    public partial class Products
+    {
+        public Products()
+        {
+
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Articles.Add(new() { Active = true, Amount = 12, Id = 1, IsInVending = false, Name = "Freistädter Märzen", Price =  new() { Amount = 1.2} });
+        }
+
+        public List<Article> Articles { get; set; } = new List<Article>();
+        public List<Article> Cart { get; set; } = new();
+
+        void AddToCart(Article article) 
+        {
+            var fArt = Cart.FirstOrDefault(x =>  x.Name == article.Name);
+            if (fArt != null) fArt.Amount++;
+            else Cart.Add(article);
+        }
+
+        void RemoveFromCart(Article article)
+        {
+            var fArt =  Cart.FirstOrDefault(x => x.Name ==article.Name);
+            if(fArt != null) fArt.Amount--;
+            else Cart.Remove(article);
+        }
+    }
+}
