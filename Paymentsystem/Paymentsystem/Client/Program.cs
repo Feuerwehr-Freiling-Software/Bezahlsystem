89982df8fa75205256a28f4d77acca779b61334f
HaunBa <haunschmied.b@gmail.com>
2022-12-19 18:50:52 +0100

    Installed Mudblazor

--- Paymentsystem/Paymentsystem/Client/Program.cs
+++ Paymentsystem/Paymentsystem/Client/Program.cs
@@ -2,12 +2,14 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using Paymentsystem.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
