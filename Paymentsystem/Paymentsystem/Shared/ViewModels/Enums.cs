81155ab3bc7a2ca89aae4e98229be1d4f7a9b3df
HaunBa <haunschmied.b@gmail.com>
2023-01-02 12:36:05 +0100

    Updated Db Models

---
+++ Paymentsystem/Paymentsystem/Shared/ViewModels/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paymentsystem.Shared.ViewModels
+{
+    public class Enums
+    {
+        public enum Importance
+        {
+            High = 0,
+            Medium = 1,
+            Low = 2
+        }
+
+        public enum LogSeverity
+        {
+            Info,
+            Warning,
+            Error                
+        }
+    }
+}
