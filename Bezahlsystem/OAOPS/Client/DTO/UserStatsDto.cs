a218ec91ea58ad6404a747da27e45829ff744d45
HaunBa <haunschmied.b@gmail.com>
2023-10-02 15:31:36 +0200

    Added pager

---
+++ Bezahlsystem/OAOPS/Client/DTO/UserStatsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace OAOPS.Client.DTO
+{
+    public class UserStatsDto
+    {
+        public Dictionary<string, int> ArticleStats { get; set; } = new();
+        public Dictionary<string, List<double>> BalanceStats { get; set; } = new();
+    }
+}
---
+++ Bezahlsystem/OAOPS/Client/Pages/UserProfile.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace OAOPS.Client.Pages
+{
+    public partial class UserProfile
+    {
+        public UserProfile()
+        {
+
+        }
+
+        async Task GetUserStats()
+        {
+            UserStatsDto res = await DataService.GetUserStats();
+
+        }
+    }
+}
